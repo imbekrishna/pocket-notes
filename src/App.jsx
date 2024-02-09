18af1b0a7a4b6752e6f778932077f3f13f0b95db
imbekrishna <136263294+imbekrishna@users.noreply.github.com>
2024-02-09 11:29:52 +0530

    refactored sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import './App.css';
-import { groups } from './utils/data';
-import { getGroupInitials } from './utils/helpers';
-
+import Sidebar from './components/Sidebar';
 function App() {
-  return (
-    <main className="sidebar">
-      <h1 className="sidebar__header">Pocket Notes</h1>
-      <div className="sidebar__grps">
-        <ul className="grps__container">
-          {groups.map((group) => (
-            <li key={group.id} className="grp__item">
-              <span className="grp__logo">{getGroupInitials(group.name)}</span>
-              <span className="grp__name">{group.name}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <button className="btn-fab">+</button>
-    </main>
-  );
+  return <Sidebar />;
 }
 
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { groups } from "../utils/data";
+import { getGroupInitials } from "../utils/helpers";
+
+const Sidebar = () => {
+  return (
+    <main className="sidebar">
+      <h1 className="sidebar__header">Pocket Notes</h1>
+      <div className="sidebar__grps">
+        <ul className="grps__container">
+          {groups.map((group) => (
+            <li key={group.id} className="grp__item">
+              <span className="grp__logo">{getGroupInitials(group.name)}</span>
+              <span className="grp__name">{group.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className="btn-fab">+</button>
+    </main>
+  );
+}
+
+export default Sidebar
